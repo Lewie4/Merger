fd03147e91fa163d284768a8c35dd072222be4dd
Lewis Nicholson <lewisn@exient.com>
2018-07-12 13:59:18 +0100

    Added start of story

--- Assets/Scripts/Slots/SlotManager.cs
+++ Assets/Scripts/Slots/SlotManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class SlotManager : Singleton<SlotManager> 
@@ -67,19 +66,31 @@ public class SlotManager : Singleton<SlotManager>
 			Debug.Log(string.Format("Slot 1: {0}, Slot 2: {1}", m_mergeSlots[0].GetElement().m_id, m_mergeSlots[1].GetElement().m_id));
 			var createdElement = m_elementManager.CheckMerge(m_mergeSlots[0].GetElement(), m_mergeSlots[1].GetElement());
 
-			if(createdElement != null)
+			if(createdElement != null && !m_elementManager.CheckCollected(createdElement))
 			{
-				Debug.Log(string.Format("You created: {0}!", createdElement.m_id));
-
-				if(!m_elementManager.CheckCollected(createdElement))
-				{
-					m_elementManager.CollectElement(createdElement);
-					var slot = CreateInventorySlot(createdElement);
-					slot.GetElementHolder().PlaceResult();
-				}
-
+				ElementCreated(createdElement);
+			}
+			else
+			{
+				NoElementCreated();
 			}
 			
 		}
+	}
+
+	private void ElementCreated(BaseElement createdElement)
+	{
+		//Story
+		StoryManager.Instance.NewElementCreated(createdElement);
+		
+		//Add Element
+		m_elementManager.CollectElement(createdElement);
+		var slot = CreateInventorySlot(createdElement);
+		slot.GetElementHolder().PlaceResult();
+	}
+	
+	private void NoElementCreated()
+	{
+		//Show no element created animation on result slot
 	}
 }
---
+++ Assets/Scripts/Story/StoryManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryManager : Singleton<StoryManager> 
+{
+	[Header("Background")]
+	[SerializeField] private GameObject m_storyOverlay;
+	[SerializeField] private Image m_background;
+	
+	[Header("Text")]
+	[SerializeField] private TextMeshProUGUI m_text;
+
+	[Header("Image")]
+	[SerializeField] private Image m_elementImage;
+
+	[Header("Settings")]
+	[SerializeField] private float m_fadeInTime = 0.5f;
+
+	private float m_timeStartedLerp;
+	private bool m_fadeIn;
+
+	private void Start()
+	{
+		m_storyOverlay.SetActive(false);
+	}
+
+	private void Update()
+	{
+		if (m_fadeIn)
+		{
+			float timeSinceStarted = Time.time - m_timeStartedLerp;
+			float percentageComplete = timeSinceStarted / m_fadeInTime;
+			
+			FadeAllElements(percentageComplete);
+			
+			if (percentageComplete >= 1)
+			{
+				m_fadeIn = false;
+			}
+		}
+	}
+
+	public void NewElementCreated(BaseElement element)
+	{
+		m_storyOverlay.SetActive(true);
+		m_text.text = element.m_description;
+		m_elementImage.sprite = element.m_icon;
+		
+		StartFadeIn();
+	}
+
+	private void StartFadeIn()
+	{
+		m_fadeIn = true;
+		m_timeStartedLerp = Time.time;
+		FadeAllElements(0);
+	}
+
+	private void FadeAllElements(float percentage)
+	{
+		m_background.color = FadeAlpha(m_background.color, percentage);
+		m_text.color = FadeAlpha(m_text.color, percentage);
+		m_elementImage.color = FadeAlpha(m_elementImage.color, percentage);
+	}
+	
+
+	private Color FadeAlpha(Color colour, float percentage)
+	{
+		Color tempColour = colour;
+		tempColour.a = percentage;
+		return tempColour;
+	}
+
+	public void Dismiss()
+	{
+		if (!m_fadeIn)
+		{
+			m_storyOverlay.SetActive(false);
+		}
+	}
+}
